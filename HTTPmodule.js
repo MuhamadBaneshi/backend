3723b33c39692403014e721ff5f91ac2442feef4
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2022-12-30 14:58:25 +0330

    Update HTTPmodule.js

--- HTTPmodule.js
+++ HTTPmodule.js
@@ -1,10 +1,15 @@
 const http = require('http');
 
-const server = http.createServer();
-
-server.on('connection', (socket) => {
-  console.log('New connection...');
+const server = http.createServer((req, res) => {
+  if(req.url === '/'){
+    res.write('Hello World');
+    res.end();
+  }
 });
+
+//server.on('connection', (socket) => {
+//  console.log('New connection...');
+//});
 
 server.listen(3000);
 
