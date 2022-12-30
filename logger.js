274a5f42fa1a1345019026df103f4ab7533e1ff7
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2022-12-30 13:41:30 +0330

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,7 +1,14 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
 var url = 'http://myLogger.io/log';
+
 function log(message){
     // send an HTTP request
     console.log(message);
+    
+    //Raise an event
+    emitter.emit('messageLogged', {id: 1, url: 'http://' });
 }
 
 module.exports.log = log;
