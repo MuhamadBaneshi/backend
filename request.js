9a60d473cefd9e7a0594833f8daa13407f6b85ea
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2022-12-31 13:26:32 +0330

    Update request.js

--- request.js
+++ request.js
@@ -1,1 +1,23 @@
 
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+
+  const { error } = validateCourse(req.body);
+  if(error) return res.status(400).send(error.details[0].message);
+
+  course.name = req.body.name;
+  res.send(course);
+ });
+
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  
+  res.send(course);
+});
