a5662641654303d9f281a292085ec31f9fe98687
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2022-12-31 13:47:31 +0330

    Update request.js

--- request.js
+++ request.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const courses = [
-  { id:1, name: 'course1' }
-  { id:2, name: 'course2' }
-  { id:3, name: 'course3' }
+  { id: 1, name: 'course1' }
+  { id: 2, name: 'course2' }
+  { id: 3, name: 'course3' }
   ];
 
 app.get('/', (req, res) => {
