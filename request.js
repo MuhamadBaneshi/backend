4d134b5c67da6b45fb59443cb3370374af7d3b4d
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2022-12-31 13:28:47 +0330

    Update request.js

--- request.js
+++ request.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
 
 app.put('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
