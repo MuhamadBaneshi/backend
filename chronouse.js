43c320af0ecf8fb181b02b5f04d1b90e87c32fe3
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2023-01-02 11:47:06 +0330

    Update chronouse.js

--- chronouse.js
+++ chronouse.js
@@ -6,7 +6,15 @@ console.log('After');
 
 function getUser(id, callback){
   setTimeout(() => {
-  console.log('Reading a user from a database ...');
-    callback ({ id: id, gitHubUsername: 'muhamad' });
+  console.log('Reading a user from a database...');
+    callback({ id: id, gitHubUsername: 'muhamad' });
 }, 2000);
+}
+
+function getRepositories(username){
+  setTimeout(() => {
+    console.log('Calling Github API...');
+  }, 2000);
+  
+ return ['repo1', 'repo2', 'repo3'] 
 }
