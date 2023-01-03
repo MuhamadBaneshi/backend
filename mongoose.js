1f2a91c8ca01512e5a5b3834546c9e3cbed9affe
MuhamadBaneshi <43133448+MuhamadBaneshi@users.noreply.github.com>
2023-01-03 12:03:25 +0330

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -40,3 +40,16 @@ async function getCourses(){
 }
 
 getCourses();
+
+async function updateCourse(id){
+ const course = await Course.findById(id);
+  if (!course) return;
+  
+  course.isPublished = true;
+  course.author = 'Another Author';
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+updateCourse('56563163');
